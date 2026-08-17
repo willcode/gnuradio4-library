1f40536ee0d1606d35a6a764aa539e6b591bebe8
Jeff Long <willcode4@gmail.com>
2026-08-17 09:38:48 -0400

    cmake: declare gnuradio-algorithm's dependency on gnuradio-core
    The public headers of gnuradio-algorithm include DataSet.hpp,
    MemoryAllocators.hpp, Message.hpp, HistoryBuffer.hpp, SVD.hpp, Tensor.hpp
    and TensorMath.hpp, but the INTERFACE link list named only
    gnuradio-options, gnuradio-meta, vir and magic_enum. Consumers linking
    gnuradio-algorithm alone got no include path for those headers and built
    only because they linked gnuradio-core for other reasons.
    
    The exported package config already calls find_dependency(gnuradio4), so
    the added gnuradio4::gnuradio-core resolves for installed consumers too.
    
    Signed-off-by: Jeff Long <willcode4@gmail.com>

--- cmake/GnuRadioTestHelpers.cmake
+++ cmake/GnuRadioTestHelpers.cmake
@@ -1,6 +1,12 @@
 function(setup_test_no_asan TEST_NAME)
   target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_BINARY_DIR}/include ${CMAKE_CURRENT_BINARY_DIR})
-  target_link_libraries(${TEST_NAME} PRIVATE gnuradio-options gnuradio-core gnuradio-blocklib-core ut ${GR_TEST_HELPER_LIBRARIES})
+  target_link_libraries(
+    ${TEST_NAME}
+    PRIVATE gnuradio-options
+            gnuradio-core
+            gnuradio-blocklib-core
+            ut
+            ${GR_TEST_HELPER_LIBRARIES})
   add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} ${CMAKE_CURRENT_BINARY_DIR}/${TEST_NAME})
 endfunction()
 
@@ -11,6 +17,7 @@ endfunction()
 function(add_ut_test TEST_NAME)
   add_executable(${TEST_NAME} ${TEST_NAME}.cpp)
   setup_test(${TEST_NAME})
-  set_property(TEST ${TEST_NAME} PROPERTY ENVIRONMENT_MODIFICATION "GNURADIO4_PLUGIN_DIRECTORIES=set:${CMAKE_CURRENT_BINARY_DIR}/plugins")
+  set_property(TEST ${TEST_NAME} PROPERTY ENVIRONMENT_MODIFICATION
+                                          "GNURADIO4_PLUGIN_DIRECTORIES=set:${CMAKE_CURRENT_BINARY_DIR}/plugins")
   target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_CURRENT_FUNCTION_LIST_DIR})
 endfunction()
